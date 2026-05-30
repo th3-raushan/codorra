8854c7d7fe4948546ce6d54018c6ec564d2995a8
Raushan kumar <raushankr1903@gmail.com>
2026-05-30 08:54:33 +0530

    Designed and built SignIn Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import HomePage from './components/HomePage/HomePage';
 import VerificationPage from './components/VerificationPage/VerificationPage';
 import LandingPage from './components/LandingPage/LandingPage';
 import SignUp from './components/SignUp/SignUp';
-
+import SignIn from './components/SignIn/SignIn';
 
 const App = () => (
     <div className="app">
@@ -11,7 +11,8 @@ const App = () => (
         <Route path="/" element={<LandingPage/>}/>
         <Route path="/verification" element={<HomePage />} />
         <Route path="/results" element={<VerificationPage />} />
-        <Route path='/signUp' element={<SignUp/>} />
+        <Route path='/signin' element={<SignIn/>} />
+        <Route path='/signup' element={<SignUp/>} />
       </Routes>
     </div>
 );
