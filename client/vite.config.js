8898f8c8b34df9dc52a3f00d5c7c5c6b443d1f89
Raushan kumar <raushankr1903@gmail.com>
2026-05-31 12:37:12 +0530

    Added server deployed link in frontend ✅

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://codorra-server.onrender.com',
         changeOrigin: true,
         timeout: 300000,
       },
